b98aaeb7c61ac5a3d53dfbe0c137ac9a76f2b3f0
Dmytro Hordon <dmytro@preen.me>
2020-01-15 23:08:10 +0200

    app factory created

---
+++ src/main.py
@@ -0,0 +1,25 @@
+from aiohttp import web
+
+from .db import create_db
+from . import settings
+
+
+async def on_startup(app):
+    app['db'] = await create_db(app)
+
+
+async def close_db(app):
+    engine = app['db']
+    engine.close()
+    await engine.wait_closed()
+
+
+def init_app(*args, **kwargs):
+
+    app = web.Application()
+    app['config'] = settings.config
+
+    app.on_startup.append(on_startup)
+    app.on_cleanup.append(close_db)
+
+    web.run_app(app)

3c374e7a817034b7e4ce5c7184c8fba144471cf1
Dmytro Hordon <dmytro@preen.me>
2020-01-15 23:43:21 +0200

    added trimming for string fields

--- src/schemas.py
+++ src/schemas.py
@@ -3,8 +3,15 @@ from marshmallow import Schema, fields, validate, EXCLUDE
 from .db import RECORD_NAME_MAX_LENGTH
 
 
+class TrimmedString(fields.String):
+    def _deserialize(self, value, *args, **kwargs):
+        if hasattr(value, 'strip'):
+            value = value.strip()
+        return super()._deserialize(value, *args, **kwargs)
+
+
 class UserRecordSchema(Schema):
-    name = fields.Str(required=True, validate=validate.Length(max=RECORD_NAME_MAX_LENGTH))
+    name = TrimmedString(required=True, validate=validate.Length(max=RECORD_NAME_MAX_LENGTH))
     age = fields.Integer(required=True)
 
 

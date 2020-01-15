db5f451b022cb31e2e38f3e674e30ad5488788f4
Dmytro Hordon <dmytro@preen.me>
2020-01-16 01:54:27 +0200

    added cors in debug mode

--- src/routes.py
+++ src/routes.py
@@ -1,9 +1,10 @@
 from aiohttp import web
 
 from .views import RecordHandler
-
 from .usecases import UserRecordUsecase
 from .db import records
+from .utils import setup_cors
+from . import settings
 
 
 def setup(app: web.Application):
@@ -11,4 +12,10 @@ def setup(app: web.Application):
     record_handler = RecordHandler(
         record_usecase=UserRecordUsecase(records, app)
     )
-    app.router.add_post('/api/v1//submit-record/', record_handler.submit_data)
+    # app.router.add_get('/', record_handler.index)
+    app.router.add_post('/api/v1/submit-record/', record_handler.submit_data)
+
+    if settings.DEBUG:
+        setup_cors(app)
+
+

cdd00324e4c78827e855496f38f7902221319086
Dmytro Hordon <dmytro@preen.me>
2020-01-15 23:40:27 +0200

    created UserRecordUsecase

---
+++ src/usecases.py
@@ -0,0 +1,18 @@
+
+class UserRecordUsecase:
+
+    def __init__(self, record_model, app):
+        self.record_model = record_model
+        self.app = app
+
+    @property
+    def db(self):
+        return self.app['db']
+
+    async def create(self, data):
+        async with self.db.acquire() as conn:
+            # I was trying to return id but I failed.
+            # get sqlalchemy.exc.CompileError: RETURNING is not supported by this dialect's statement compiler
+            # need to research more
+            result = await conn.execute(self.record_model.insert().values(**data))
+            await conn.execute('commit')
